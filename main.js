79c18c5b04850da1d009bf589cb67ddc7baf9796
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-03-18 12:17:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 // JS Assessment: Find Your Hat //
 const prompt = require("prompt-sync")({ sigint: true });
 const clear = require("clear-screen");
+
+const hat = "^";
 const hole = "O";
 const fieldCharacter = "░";
 const pathCharacter = "*";
@@ -9,10 +11,12 @@ const pathCharacter = "*";
 class Field {
 	constructor(field = [[]]) {
 		this.field = field;
+
+		// Replace with your own code //
+		// Set the home position at (0, 0) before the game starts
 		this.positionRow = 0;
 		this.positionCol = 0;
-		// Set the home position at (0, 0) before the game starts
-		this.field[this.positionCol][this.positionRow] = pathCharacter;
+		this.field[this.positionRow][this.positionCol] = pathCharacter;
 	}
 
 	// Print field method to make it eaier
@@ -21,6 +25,8 @@ class Field {
 		// Your print map code here
 		// YOUR CODE
 		console.log(this.field); // Please REMOVE this line before you start your code!
+		console.log(this.field[this.positionRow][this.positionCol]); // Please REMOVE this line before you start your code!
+		console.log(this.field[2][this.positionCol]); // Please REMOVE this line before you start your code!
 	}
 
 	// The rest of your code starts here
@@ -30,7 +36,7 @@ class Field {
 // Game Mode ON
 // Remark: Code example below should be deleted and use your own code.
 const newGame = new Field([
-	["*", "░", "O"],
+	["░", "░", "O"],
 	["░", "O", "░"],
 	["░", "^", "░"],
 ]);
